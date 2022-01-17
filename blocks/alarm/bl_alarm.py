8df114fed1e16aa91b91f034df93a561b88e48b8
a.dzenisevich <andrej.dzenisevich@innowise-group.com>
2022-01-17 21:33:40 +0300

    alarm

--- blocks/alarm/bl_alarm.py
+++ blocks/alarm/bl_alarm.py
@@ -3,10 +3,9 @@ from widgetblocks.widgetblock import WidgetBlock
 
 class Alarm(WidgetBlock):
     def __init__(self, widget, d_time=1000):
+        super().__init__()
         self.widget = widget
         self.delta_time = d_time
-        
-        
         
     def alarm(self):
         self.widget.after(self.delta_time, self.alarm)
